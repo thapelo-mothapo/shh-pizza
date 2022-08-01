04f8ac50723c70af30aeb9dfb67af6dc03a73ddb
Thapelo <dev.mothapotr@gmail.com>
2022-08-01 09:53:52 +0200

    feat(navigation): add items to navbar

--- src/containers/Navigation/index.tsx
+++ src/containers/Navigation/index.tsx
@@ -1,14 +1,26 @@
+import { Col, Row } from "antd";
 import { Wrapper } from "./styles";
 
 const NavBar: React.FC = () => {
   return (
-    <Wrapper>
-      <img
-        alt=""
-        src="https://romanspizza.co.za/static/media/logo-shield-large.ac2e904a.png"
-        style={{ height: "80px", width: "80px" }}
-      />
-    </Wrapper>
+    <Row align="middle">
+      <Col span={8}>
+        <Wrapper>
+          <img
+            alt=""
+            src="https://romanspizza.co.za/static/media/logo-shield-large.ac2e904a.png"
+            style={{ height: "80px", width: "80px" }}
+          />
+        </Wrapper>
+      </Col>
+      <Col span={16}>
+        <Row justify="end">
+          <Col span={4}>Order Now</Col>
+          <Col span={4}>Menu</Col>
+          <Col span={4}>Login / SignUp</Col>
+        </Row>
+      </Col>
+    </Row>
   );
 };
 
